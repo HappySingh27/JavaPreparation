bf4fb00ffbacd6c209e1864f9a2ce504b1eb9fa1
Happy Singh <33799450+HappySingh27@users.noreply.github.com>
2025-02-25 22:19:56 +0530

    Update-ALremoveDupsSortandReverse-1

--- src/main/java/com/tech/prep/collections/ArrayList.java
+++ src/main/java/com/tech/prep/collections/ArrayList.java
@@ -1,5 +1,40 @@
 package com.tech.prep.collections;
 
+import java.util.*;
+
 public class ArrayList {
+
+  public static void main(String[] args){
+
+    
+  }
+
+  /*
+  This method would do the following
+    a. Convert a array into ArrayList.
+    b. Remove duplicates from ArrayList.
+    c. Count frequency of each element in ArrayList.
+    d. Sort ArrayList
+    e. Reverse ArrayList
+  */
+  pubic void ALremoveDupsSortandReverse(){
+
+        Integer[] a = {23,23,5,1,2,398,89,34,56,65,5,398,1};
+        
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(a));
+        Set<Integer> lhset = new LinkedHashSet<>();
+        
+        lhset.addAll(list);
+        
+        list.clear();
+        
+        list.addAll(lhset);
+        
+        Collections.reverse(list);
+        Collections.sort(list);
+        
+        System.out.println(list);
+    
+  }
 
 }
