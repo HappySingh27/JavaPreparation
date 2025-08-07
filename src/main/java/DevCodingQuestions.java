781f01a657a76c0ad753d27bd705163556ca4a70
Happy Singh <happysingh@Happys-MacBook-Air.local>
2025-08-07 22:01:49 +0530

    fixed substring max number

--- src/main/java/DevCodingQuestions.java
+++ src/main/java/DevCodingQuestions.java
@@ -219,5 +219,74 @@ public class DevCodingQuestions {
 
 		System.out.println(str.substring(s, s + size));
 	}
+	
+    public static void longestSubstringWithEachCharacterRepeatingAtomstKtimes(String[] args) {
+        
+        //Longest Substring with All characters repeating at most k times
+        // Example to test:
+        // Input:  "aaabbc", k = 2
+        // Expected Output: "aabbc" (length = 5)
+        
+        String str = "aaabbc";
+        
+        int l=0;
+        int s=0;
+        int k=2;
+        int size=0;
+        
+        Map<Character,Integer> map = new HashMap();
+        
+        for(int r=0;r<str.length();r++){
+            char ch = str.charAt(r);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            
+            while(map.get(ch)>k){
+                char left = str.charAt(l);
+               map.put(left,map.getOrDefault(left,0)-1); 
+               l++;
+            }
+            
+            if(r-l+1>size){
+                size=r-l+1;
+                s=l;
+            }
+        }
+        
+        System.out.println(str.substring(s,s+size));
+        System.out.println("Try programiz.pro");
+    }
+    
+public static void main(String[] args) {
+        
+        //int[] arr = {1,3,-1,-3,5,3,6,7};
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int k=3;
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int r=0;r<arr.length;r++){
+            
+            while(!deque.isEmpty() && deque.peekFirst()<=r-k){
+                deque.pollFirst();
+            }
+            
+            while(!deque.isEmpty() && arr[deque.peekLast()]<arr[r]){
+                System.out.println(deque);
+                deque.pollLast();
+                System.out.println(deque);
+            }
+            
+            deque.addLast(r);
+            
+            if(r>=k-1){
+                list.add(arr[deque.peekFirst()]);
+            }
+            
+        }
+        
+        System.out.println(list);
+        System.out.println("Try programiz.pro");
+    }
 
 }
